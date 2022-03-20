e1af43d5f7c47bb3fbc35177d26dc0c9f3939f74
Carlitos <calitod91@gmail.com>
2022-03-20 17:02:22 -0600

    Barra de Navegacion App Administrativa

---
+++ js/controladorAdministrativa.js
@@ -0,0 +1,7 @@
+function activarMenu(){
+	document.getElementById("Menu-Opciones-Principal").classList.toggle('Lista-Opciones-Show');
+}
+
+function generarPerfil(){
+	document.getElementById("Contenedor-Principal-Formulario").classList.toggle('Contenedor-Formulario-Show');
+}
