88e34392c186256ed4d30ef44127903b533f6448
Carlitos <calitod91@gmail.com>
2022-03-20 21:06:56 -0600

    Cards Agregadas

--- js/controladorAdministrativa.js
+++ js/controladorAdministrativa.js
@@ -1,7 +1,9 @@
 function activarMenu(){
+	// document.getElementById("Contenedor-Principal-Formulario").classList.remove('Contenedor-Formulario-Show');
 	document.getElementById("Menu-Opciones-Principal").classList.toggle('Lista-Opciones-Show');
 }
 
 function generarPerfil(){
+	// document.getElementById("Menu-Opciones-Principal").classList.remove('Lista-Opciones-Show');	
 	document.getElementById("Contenedor-Principal-Formulario").classList.toggle('Contenedor-Formulario-Show');
 }
